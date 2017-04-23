a2f768acfb23fcd96bb825b2a75d0ea12b1c1f14
Ashish Negi <ashishnegi33@gmail.com>
2017-04-23 23:47:09 +0530

    Adds benchmark after tests and fixed bugs:
    qsort: RUNNING...
    benchmarking qsortSimple/4000
    time                 2.903 ms   (2.878 ms .. 2.933 ms)
                         0.998 R²   (0.995 R² .. 0.999 R²)
    mean                 2.957 ms   (2.925 ms .. 3.013 ms)
    std dev              133.3 μs   (80.91 μs .. 198.4 μs)
    variance introduced by outliers: 29% (moderately inflated)
    
    benchmarking qsortFoldr/4000
    time                 614.6 μs   (610.2 μs .. 620.2 μs)
                         0.999 R²   (0.999 R² .. 1.000 R²)
    mean                 617.0 μs   (615.6 μs .. 619.9 μs)
    std dev              6.500 μs   (3.896 μs .. 11.79 μs)
    
    benchmarking qsortFoldl/4000
    time                 514.4 μs   (502.8 μs .. 523.5 μs)
                         0.990 R²   (0.967 R² .. 0.999 R²)
    mean                 543.8 μs   (516.2 μs .. 611.1 μs)
    std dev              139.1 μs   (39.71 μs .. 274.6 μs)
    variance introduced by outliers: 96% (severely inflated)
    
    benchmarking qsortFoldl'/4000
    time                 498.7 μs   (494.3 μs .. 502.0 μs)
                         0.986 R²   (0.966 R² .. 1.000 R²)
    mean                 535.4 μs   (515.1 μs .. 581.8 μs)
    std dev              101.7 μs   (34.41 μs .. 171.3 μs)
    variance introduced by outliers: 92% (severely inflated)

--- bench/QuickSort.hs
+++ bench/QuickSort.hs
@@ -1,15 +1,71 @@
+-- module QuickSort where
 import Criterion.Main as C
+import Data.List as DL
+import System.Random as R
 
-qsort1 :: (Ord a) => [a] -> [a]
-qsort1 [] = []
-qsort1 (x:xs) =
-  qsort1 [y | y <- xs, y < x] ++
-  [y | y <- xs, y == x] ++
-  qsort1 [y | y <- xs, y > x]
+qsortSimple :: (Ord a) => [a] -> [a]
+qsortSimple [] = []
+qsortSimple (x:xs) =
+  qsortSimple [y | y <- xs, y < x] ++ [x] ++
+  qsortSimple [y | y <- xs, y >= x]
+
+qsortFoldr :: (Ord a) => [a] -> [a]
+qsortFoldr [] = []
+qsortFoldr (x:xs) = (qsortFoldr smaller) ++ same ++ (qsortFoldr greater)
+  where
+    (smaller, same, greater) = foldr (\v (sm,sa,gr) ->
+                                        if v < x
+                                        then (v:sm, sa, gr)
+                                        else if v == x
+                                        then (sm, v:sa, gr)
+                                        else (sm, sa, v:gr))
+                               ([],[x],[])
+                               xs
+
+qsortFoldl :: (Ord a) => [a] -> [a]
+qsortFoldl [] = []
+qsortFoldl (x:xs) = qsortFoldl smaller ++ same ++ qsortFoldl greater
+  where
+    (smaller, same, greater) = foldl (\(sm,sa,gr) v ->
+                                        if v < x
+                                        then (v:sm, sa, gr)
+                                        else if v == x
+                                        then (sm, v:sa, gr)
+                                        else (sm, sa, v:gr))
+                               ([],[x],[])
+                               xs
+
+qsortFoldl' :: (Ord a) => [a] -> [a]
+qsortFoldl' [] = []
+qsortFoldl' (x:xs) = qsortFoldl' smaller ++ same ++ qsortFoldl' greater
+  where
+    (smaller, same, greater) = DL.foldl' (\(sm,sa,gr) v ->
+                                        if v < x
+                                        then (v:sm, sa, gr)
+                                        else if v == x
+                                        then (sm, v:sa, gr)
+                                        else (sm, sa, v:gr))
+                               ([],[x],[])
+                               xs
+
+qsortBench :: ([Int] -> [Int]) -> [Int] -> [C.Benchmark]
+qsortBench qsort nums = [ C.bench (show (length nums)) $ C.nf qsort nums ]
 
 main :: IO ()
-main = C.defaultMain [
-  C.bgroup "qsort1" [ C.bench "1000" $ C.whnf qsort1 [1..1000 :: Int]
-                    , C.bench "10000" $ C.whnf qsort1 [1..2000 :: Int]
-                    ]
-  ]
+main = do
+  r <- R.getStdGen
+  let nums = (take 5000 $ R.randoms r) :: [Int]
+  C.defaultMain [ C.bgroup "qsortFoldl'" $ qsortBench qsortFoldl' nums
+                , C.bgroup "qsortFoldr" $ qsortBench qsortFoldr nums
+                , C.bgroup "qsortFoldl" $ qsortBench qsortFoldl nums
+                , C.bgroup "qsortSimple" $ qsortBench qsortSimple nums
+                ]
+  print $ show ((qsortSimple nums == qsortFoldr nums) &&
+                (qsortFoldr nums == qsortFoldl nums) &&
+                (qsortFoldl nums == qsortFoldl' nums))
+
+  -- print $ nums
+  -- print $ qsortSimple nums
+  -- print $ qsortFoldr nums
+  -- print $ qsortFoldl nums
+  -- print $ qsortFoldl' nums
